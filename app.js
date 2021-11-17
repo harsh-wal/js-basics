1fa626a1711e4565c2db41150deb6b557a622817
harsh-wal <harsh.d@westagilelabs.com>
2021-11-17 13:02:48 +0530

    basic js

---
+++ app.js
@@ -0,0 +1,94 @@
+// console.log('test');
+// let a;
+// const b = 1;
+
+// const person = { name: 'harsh' };
+// person.name = 'adfds';
+// console.log(person.name);
+
+// const a = [];
+
+// a.push(1);
+// a.push(2);
+// a.push('adffsads');
+// a.push({ name: 'sfds' });
+
+// let a = 1;
+// let val = false;
+// let str = 'sdafdsaf';
+// let float = 1.2;
+// let c;
+// let obj = {};
+// let arr = [];
+// let js = null;
+
+// console.log(run());
+
+// const run = () => {
+//   return 'afdas';
+// };
+
+// function run() {
+//   return { name: 'sadsa' };
+// }
+
+// console.log(run());
+
+// var // function
+// var // script
+
+// let, const //block scope
+
+// const a = 'abc';
+// const b = 'def';
+
+// const c = `this is a sting ${a}${b}`;
+
+// const person = { name: 'ssfd', age: 10, gender: 'f' };
+
+// const { name, ...obj } = person;
+
+// console.log(gender);
+
+// person.name;
+// person.age;
+
+// const array = [1, 2, 3, 4, 5];
+// const [one, ...anotherArray] = array;
+
+// array[0];
+// array[1];
+// array[2];
+
+// function getName({ name }) {
+//   return name;
+// }
+
+// console.log(getName(person));
+
+// for (let i = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// }
+
+// const array = [
+//   { name: 'asf', age: 10 },
+//   { name: 'safdsafd', age: 30 },
+// ];
+
+// console.log(
+//   array.forEach(({ name }) => {
+//     console.log(name);
+//   })
+// );
+
+// console.log(
+//   array.map(({ name }) => {
+//     return { name };
+//   })
+// );
+
+// for of
+// for in
+// const a = 10 > 20 ? 2 : 4;
+
+// if()
