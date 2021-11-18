56291ccdeb31ee0385461f769baa42274314b101
harsh-wal <harsh.d@westagilelabs.com>
2021-11-18 12:02:10 +0530

    updated code

--- app.js
+++ app.js
@@ -1,94 +1,85 @@
-// console.log('test');
-// let a;
-// const b = 1;
+console.log('test');
+let a;
+const b = 1;
 
-// const person = { name: 'harsh' };
-// person.name = 'adfds';
-// console.log(person.name);
+const person = { name: 'harsh' };
+person.name = 'adfds';
+console.log(person.name);
 
-// const a = [];
+const a = [];
 
-// a.push(1);
-// a.push(2);
-// a.push('adffsads');
-// a.push({ name: 'sfds' });
+a.push(1);
+a.push(2);
+a.push('adffsads');
+a.push({ name: 'sfds' });
 
-// let a = 1;
-// let val = false;
-// let str = 'sdafdsaf';
-// let float = 1.2;
-// let c;
-// let obj = {};
-// let arr = [];
-// let js = null;
+let a = 1;
+let val = false;
+let str = 'sdafdsaf';
+let float = 1.2;
+let c;
+let obj = {};
+let arr = [];
+let js = null;
 
-// console.log(run());
+console.log(run());
 
-// const run = () => {
-//   return 'afdas';
-// };
+const run = () => {
+  return 'afdas';
+};
 
-// function run() {
-//   return { name: 'sadsa' };
-// }
+function run() {
+  return { name: 'sadsa' };
+}
 
-// console.log(run());
+console.log(run());
 
-// var // function
-// var // script
+// let, const // block scope
 
-// let, const //block scope
+const a = 'abc';
+const b = 'def';
 
-// const a = 'abc';
-// const b = 'def';
+const c = `this is a sting ${a}${b}`;
 
-// const c = `this is a sting ${a}${b}`;
+const person = { name: 'ssfd', age: 10, gender: 'f' };
 
-// const person = { name: 'ssfd', age: 10, gender: 'f' };
+const { name, ...obj } = person;
 
-// const { name, ...obj } = person;
+console.log(gender);
 
-// console.log(gender);
+person.name;
+person.age;
 
-// person.name;
-// person.age;
+const array = [1, 2, 3, 4, 5];
+const [one, ...anotherArray] = array;
 
-// const array = [1, 2, 3, 4, 5];
-// const [one, ...anotherArray] = array;
+array[0];
+array[1];
+array[2];
 
-// array[0];
-// array[1];
-// array[2];
+function getName({ name }) {
+  return name;
+}
 
-// function getName({ name }) {
-//   return name;
-// }
+console.log(getName(person));
 
-// console.log(getName(person));
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
 
-// for (let i = 0; i < array.length; i++) {
-//   console.log(array[i]);
-// }
+const array = [
+  { name: 'asf', age: 10 },
+  { name: 'safdsafd', age: 30 },
+];
 
-// const array = [
-//   { name: 'asf', age: 10 },
-//   { name: 'safdsafd', age: 30 },
-// ];
+console.log(
+  array.forEach(({ name }) => {
+    console.log(name);
+  })
+);
 
-// console.log(
-//   array.forEach(({ name }) => {
-//     console.log(name);
-//   })
-// );
-
-// console.log(
-//   array.map(({ name }) => {
-//     return { name };
-//   })
-// );
-
-// for of
-// for in
-// const a = 10 > 20 ? 2 : 4;
-
-// if()
+console.log(
+  array.map(({ name }) => {
+    return { name };
+  })
+);
